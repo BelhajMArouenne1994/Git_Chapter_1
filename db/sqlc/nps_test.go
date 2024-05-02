fe22f1e6b4b87c60da952adb3a1c655a196a9588
BelhajMArouenne1994 <belhajmarwen5@gmai.com>
2024-05-02 21:43:04 +0200

    First Transaction Test_Connit_5

--- db/sqlc/nps_test.go
+++ db/sqlc/nps_test.go
@@ -3,34 +3,50 @@ package db
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
 )
 
 
 
 func TestNpsCreationTx(t *testing.T) {
 
-	argRecipient := CreateRecipientParams{
-		OcdMasterId: uuid.New().String(),
-		Username: sql.NullString{String: "TEST_RECIPIENT", Valid: true},
-		Role: sql.NullString{String: "TEST_RECIPIENT", Valid: true},
-		CreatedAt: sql.NullTime{Time: time.Now().UTC(), Valid: true},
-	}
-
 	nps := NewNps(testDB)
 
-	result, err := nps.NpsCreationTx(context.Background(), NpsCreationTxParams {
-		CreateRecipientParams: argRecipient,
-	}) 
+	errs := make(chan error)
+	results := make(chan NpsCreationTxResult)
 
-	if err!= nil {
-        t.Error(err)
-    }
-	fmt.Println(result)
+	for i:=0 ; i < 10; i++ {
+		argRecipient := CreateRecipientParams{
+			OcdMasterId: uuid.New().String(),
+			Username: sql.NullString{String: "TEST_RECIPIENT", Valid: true},
+			Role: sql.NullString{String: "TEST_RECIPIENT", Valid: true},
+			CreatedAt: sql.NullTime{Time: time.Now().UTC(), Valid: true},
+		}
+		
+		go func ()  {
+			result, err := nps.NpsCreationTx(context.Background(), NpsCreationTxParams {
+				CreateRecipientParams: argRecipient,
+			}) 
+
+			errs <- err
+			results <- result
+		} ()
+	}
+
+	for i:=0 ; i < 10; i++ {
+		err := <- errs
+		require.NoError(t, err)
+
+		results := <- results
+		require.NotEmpty(t, results.Recipient)
+	}
+
+
+
 }
 
 
